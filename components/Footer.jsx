47fa7170b8a173078209687e549016946e9354f3
adrianuscharlie <adrianus.charlie02@gmail.com>
2023-09-26 19:29:32 +0700

    fix new bugs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
                       <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                       <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                     </svg>
-                    <p>palugadasejahteragroup@gmail.com</p>
+                    <p className="md:text-base text-xs">palugadasejahteragroup@gmail.com</p>
                   </Link>
                 </li>
                 <li>
